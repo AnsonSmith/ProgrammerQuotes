7d31fd192882818cb76a2e0431af8dd735c00808
Smith, Anson <AnsonSmith@quickenloans.com>
2017-09-05 13:11:06 -0400

    fixing test

--- test/programming_quotes/web/controllers/page_controller_test.exs
+++ test/programming_quotes/web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule ProgrammingQuotes.Web.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Programmer Quotes"
   end
 end
