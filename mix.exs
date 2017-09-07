399fed7c4aadff6cfdf704b73950d71222758a9b
Smith, Anson <AnsonSmith@quickenloans.com>
2017-09-07 13:31:57 -0400

    version bump

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ProgrammingQuotes.Mixfile do
   def project do
     [
       app: :programming_quotes,
-      version: "0.0.1",
+      version: "0.0.2",
       elixir: "~> 1.4",
       elixirc_paths: elixirc_paths(Mix.env),
       compilers: [:phoenix, :gettext] ++ Mix.compilers,
